6600f4cd1281f5fecf274ddf242b895f284329cf
Nam <Namoplayer123@gmail.com>
2025-04-13 16:58:17 +0200

    LCUDriver: changed to a function that awaits a gameID as an imput v0.4

--- sheets/Scripts/LCUDriver.py
+++ sheets/Scripts/LCUDriver.py
@@ -1,36 +1,39 @@
 from lcu_driver import Connector
 
 connector = Connector()
+game_content = None  # globale Variable
 
-async def set_random_icon(connection, gameID):
-
-    # make the request to set the icon
+async def get_data(connection, gameID):
+    global game_content
     response = await connection.request('GET', f'/lol-match-history/v1/games/{gameID}')
-
     if response.status == 200:
-        # JSON-Inhalt abrufen
-        content = await response.json()
-        print(content)
+        game_content = await response.json()
+        print('Daten erfolgreich abgerufen')
     else:
         print(f'Fehler: {response.status}')
 
-# fired when LCU API is ready to be used
 @connector.ready
 async def connect(connection):
     print('LCU API is ready to be used.')
-    # check if the user is already logged into his account
     summoner = await connection.request('get', '/lol-summoner/v1/current-summoner')
     if summoner.status != 200:
-        print('Please login into your account to change your icon and restart the script...')
+        print('Bitte im Client einloggen und Script neu starten...')
     else:
         print('fetching League Client data ...')
-        await set_random_icon(connection, 7365236186)
+        game_id = input("Bitte geben Sie eine gameId ein: ")
+        await get_data(connection, game_id)
 
-
-# fired when League Client is closed (or disconnected from websocket)
 @connector.close
 async def disconnect(_):
     print('disconnected')
 
-# starts the connector
-connector.start()
+def start_connector():
+    connector.start()
+
+def get_content():
+    return game_content  # Zugriff auf globale Variable
+
+# Start
+start_connector()
+# Beispiel: irgendwo anders im Code (funktioniert aber nur sinnvoll asynchron)
+# print(get_content())
